cdac2bf3d57cee8a04cc47801e458365b63dfd32
Brandon Bolton <email@BrandonBolton.com>
2025-09-08 07:58:18 -0500

    Matter Server for home assistant

--- Kubernetes/DeployManifests.sh
+++ Kubernetes/DeployManifests.sh
@@ -1,25 +1,10 @@
-# Secrets, pulled from 1Password
-POSTGRES_PASSWORD=$(op read "op://Private/Kubernetes Homelab/AWXAdminPassword")
-kubectl delete secret awx-postgres-secret -n awx
-kubectl create secret generic awx-postgres-secret --namespace awx --from-literal=POSTGRES_PASSWORD="$POSTGRES_PASSWORD"
-
-GRAFANA_ADMIN_PASSWORD=$(op read "op://Private/Kubernetes Homelab/GrafanaAdminPassword")
-kubectl delete secret grafana-admin-secret -n monitoring
-kubectl create secret generic grafana-admin-secret --namespace monitoring --from-literal=GF_SECURITY_ADMIN_PASSWORD="$GRAFANA_ADMIN_PASSWORD"
-
-MYSQL_ROOT_PASSWORD=$(op read "op://Private/Kubernetes Homelab/MySQLAdminPassword")
-kubectl delete secret mysql-root-secret -n mysql
-kubectl create secret generic mysql-root-secret --namespace mysql --from-literal=MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD"
-
-# Standalone Postgres Secret
-STANDALONE_POSTGRES_PASSWORD=$(op read "op://Private/Kubernetes Homelab/PostgressPassword")
-kubectl delete secret postgres-secret -n postgres
-kubectl create secret generic postgres-secret --namespace postgres --from-literal=POSTGRES_PASSWORD="$STANDALONE_POSTGRES_PASSWORD"
-
-# Deployments
 echo "Deploying services..."
 
 echo "Deploying Grafana..."
+
+GRAFANA_ADMIN_PASSWORD=$(op read "op://Private/Kubernetes Homelab/GrafanaAdminPassword")
+kubectl delete secret grafana-admin-secret -n monitoring --ignore-not-found
+kubectl create secret generic grafana-admin-secret --namespace monitoring --from-literal=GF_SECURITY_ADMIN_PASSWORD="$GRAFANA_ADMIN_PASSWORD"
 kubectl apply -f ./Kubernetes/Grafana/
 if kubectl rollout status deployment/grafana -n monitoring; then
 	echo "Grafana deployed and ready."
@@ -28,7 +13,29 @@ else
 fi
 echo "-----------------------------------"
 
+echo "Deploying Pi-hole..."
+
+kubectl get namespace pihole || kubectl create namespace pihole
+
+PIHOLE_PRIVATE_KEY=$(op read "op://Private/Home Assistant - PiHole/private key")
+PIHOLE_PUBLIC_KEY=$(op read "op://Private/Home Assistant - PiHole/public key")
+PIHOLE_WEBPASSWORD=$(op read "op://Private/Kubernetes Homelab/PiHoleAdminPassword")
+kubectl delete secret pihole-webpassword -n pihole --ignore-not-found
+kubectl create secret generic pihole-webpassword \
+	--from-literal=WEBPASSWORD="$PIHOLE_WEBPASSWORD" \
+	-n pihole
+
+kubectl apply -f ./Kubernetes/PiHole/
+if kubectl rollout status deployment/pihole -n pihole; then
+	echo "Pi-hole deployed and ready."
+else
+	echo "Pi-hole deployment failed or not ready." >&2
+fi
+echo "-----------------------------------"
+
+
 echo "Deploying Prometheus..."
+
 kubectl apply -f ./Kubernetes/Prometheus/
 if kubectl rollout status deployment/prometheus -n monitoring; then
 	echo "Prometheus deployed and ready."
@@ -37,7 +44,12 @@ else
 fi
 echo "-----------------------------------"
 
+
 echo "Deploying MySQL..."
+
+MYSQL_ROOT_PASSWORD=$(op read "op://Private/Kubernetes Homelab/MySQLAdminPassword")
+kubectl delete secret mysql-root-secret -n mysql --ignore-not-found
+kubectl create secret generic mysql-root-secret --namespace mysql --from-literal=MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD"
 kubectl apply -f ./Kubernetes/MySQL/
 if kubectl rollout status deployment/mysql -n mysql; then
 	echo "MySQL deployed and ready."
@@ -46,12 +58,17 @@ else
 fi
 echo "-----------------------------------"
 
+
 echo "Deploying Postgres..."
+
+STANDALONE_POSTGRES_PASSWORD=$(op read "op://Private/Kubernetes Homelab/PostgressPassword")
+kubectl delete secret postgres-secret -n postgres --ignore-not-found
+kubectl create secret generic postgres-secret --namespace postgres --from-literal=POSTGRES_PASSWORD="$STANDALONE_POSTGRES_PASSWORD"
 kubectl apply -f ./Kubernetes/Postgres/
 if kubectl rollout status deployment/postgres -n postgres; then
-    echo "Postgres deployed and ready."
+	echo "Postgres deployed and ready."
 else
-    echo "Postgres deployment failed or not ready." >&2
+	echo "Postgres deployment failed or not ready." >&2
 fi
 echo "-----------------------------------"
 
@@ -66,6 +83,16 @@ if kubectl rollout status deployment/home-assistant -n home-assistant; then
 	echo "Home Assistant deployed and ready."
 else
 	echo "Home Assistant deployment failed or not ready." >&2
+fi
+echo "-----------------------------------"
+
+echo "Deploying Matter Server..."
+kubectl get namespace matter-server || kubectl create namespace matter-server
+kubectl apply -n matter-server -f ./Kubernetes/MatterServer/
+if kubectl rollout status deployment/matter-server -n matter-server; then
+	echo "Matter Server deployed and ready."
+else
+	echo "Matter Server deployment failed or not ready." >&2
 fi
 echo "-----------------------------------"
 
